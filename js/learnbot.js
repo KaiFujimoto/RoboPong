0da83b9bdc8a44440d99f94fc5a317ad9ee60109
Kai Chen <kaifujidesu@gmail.com>
2018-03-28 19:15:33 -0400

    Making learning bot chan

--- js/learnbot.js
+++ js/learnbot.js
@@ -1,26 +1,46 @@
-import * as dl from 'deeplearn';
-/**
- * We want to learn the coefficients that give correct solutions to the
- * following quadratic equation:
- *      y = a * x^2 + b * x + c
- * In other words we want to learn values for:
- *      a
- *      b
- *      c
- * Such that this function produces 'desired outputs' for y when provided
- * with x. We will provide some examples of xs and ys to allow this model
- * to learn what we mean by desired outputs and then use it to produce new
- * values of y that fit the curve implied by our example.
- */
+const Paddle = require('/paddle_main');
+const brain = require('brain.js');
 
 
- // Step 1. Set up variables, these are the things we want the model
- // to learn in order to do prediction accurately. We will initialize
- // them with random values.
- const a = dl.variable(dl.scalar(Math.random()));
- const b = dl.variable(dl.scalar(Math.random()));
- const c = dl.variable(dl.scalar(Math.random()));
+// so basically the machine learning guy (just taking a step back) is just a freakin paddle
+// and this paddle like learns to move with the stuff and not lose
 
- // Step 2. Create an optimizer, we will use this later
-const learningRate = 0.01;
-const optimizer = dl.train.sgd(learningRate);
+// need a way to punish bot chan for losing and reward bot chan for winning?
+// where would i keep that stat?
+// need a picture of the whole board somehow and the whole game or something
+// WTFFFFFF :(
+
+// i need a live update thing
+
+class LearnBotChan extends Paddle {
+  constructor(hit = 1, miss = -5) {
+    super();
+    this.brain = new brain.NeuralNetwork();
+    this.data = [{
+        input: {
+           predicted_pos: 350,
+           padd_y_pos: 250
+         },
+        output: {
+           down: 1
+         }
+       },
+       {
+         input: {
+           predicted_pos: 150,
+           padd_y_pos: 250
+         },
+         output: {
+           up: 1
+         }
+       }
+    ];
+// i need data on the game, the ball position
+  }
+
+  training() {
+    this.brain.train(this.data);
+  }
+
+
+}
